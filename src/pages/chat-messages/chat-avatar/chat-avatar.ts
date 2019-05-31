7422728a3108f0e7d5978972a681f9671d1fa0ef
William R <wiribasczky@gmail.com>
2019-05-31 06:36:12 -0300

    Ajustes dos elementos do chat
    -Construido pipe para formatar
    url da foto, vindo da API.
    
    -Componente Chat Avatar, modificado,
    permitindo definir posicao da foto,
    na direita ou na esquerda.
    
    -Modificado chat avatar, para exibir
    a foto a foto padrão, enquanto não é
    carregado a foto do usuário.

--- src/pages/chat-messages/chat-avatar/chat-avatar.ts
+++ src/pages/chat-messages/chat-avatar/chat-avatar.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the ChatAvatarComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+const DEFAULT_PHOTO_URL = 'https://gravatar.com/avatar/nouser.jpg';
 @Component({
   selector: 'chat-avatar',
   templateUrl: 'chat-avatar.html'
 })
 export class ChatAvatarComponent {
 
-  text: string;
+
+  private _photo:string = DEFAULT_PHOTO_URL;
+
+  @Input()
+  position: string;
 
   constructor() {
-    console.log('Hello ChatAvatarComponent Component');
-    this.text = 'Hello World';
+
   }
 
+  @Input()
+  set photo(value){
+    if(!value){
+      this._photo = DEFAULT_PHOTO_URL;
+      return;
+    }
+    this._photo = value;
+  }
+
+  get photo(){
+    return this._photo
+  }
 }
