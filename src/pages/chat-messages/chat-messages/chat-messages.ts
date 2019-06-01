63a6ea6e5fbc05678dbcb3cb481af1886f703ff5
William R <wiribasczky@gmail.com>
2019-06-01 02:13:09 -0300

    Modificado sincronizacao de usuarios das mensagens
    -Modificado sincronizacao de usuarios, de
    promise para observable, para que modificacoes
    realizadas no banco sejam refletidas no chat

--- src/pages/chat-messages/chat-messages/chat-messages.ts
+++ src/pages/chat-messages/chat-messages/chat-messages.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseAuthProvider } from '../../../providers/auth/firebase-auth';
 import { ChatMessage } from '../../../app/model';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -25,10 +26,10 @@ export class ChatMessagesPage {
 
     database.ref('chat_groups/1/messages').on('child_added', (data) => {
       const message = data.val();
-      message.user = new Promise((resolve) => {
+      message.user = new Observable((observer) => {
         database.ref(`users/${message.user_id}`).on('value', (data) => {
           const user = data.val();
-          resolve(user);
+          observer.next(user);
         });
       });
       this.messages.push(message);
