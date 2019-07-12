3ebdf3a95508d9dd7b3ec522f26d4d8555b64f3e
William R <wiribasczky@gmail.com>
2019-07-12 09:01:29 -0300

    Adicionado ação de cancelar pedido
    Adicionado botão para cancelar pedido, na página de detalhes do pedido

--- src/pages/order-detail/order-detail.ts
+++ src/pages/order-detail/order-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { Order, OrderStatus } from '../../app/model';
+import { OrderHttpProvider } from '../../providers/http/order-http';
 
 /**
  * Generated class for the OrderDetailPage page.
@@ -19,12 +20,38 @@ export class OrderDetailPage {
   order: Order;
   STATUS_ENUM = OrderStatus;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private loadingCtrl: LoadingController,
+    private orderHttp: OrderHttpProvider,
+    private toastCtrl: ToastController
+    ) {
     this.order = this.navParams.get('order');
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrderDetailPage');
+
+  }
+
+
+  cancel(){
+    const loader = this.loadingCtrl.create({
+      content: "Carregando..."
+    });
+    loader.present();
+    this.orderHttp.cancel(this.order.id).subscribe((order) => {
+      this.order = order;
+      loader.dismiss();
+      const toast = this.toastCtrl.create({
+        message: "Pedido cancelado com sucesso",
+        duration: 7000
+      });
+      toast.present();
+    },
+    error => {
+      loader.dismiss();
+    })
   }
 
 }
